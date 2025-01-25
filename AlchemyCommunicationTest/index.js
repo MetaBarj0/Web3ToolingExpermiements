9264ffe1a6aba74d3b6d34d8cc25cd5767b2cd65
MetaBarj0 <sebastien.levy.entrepreneur@gmail.com>
2025-01-25 13:56:00 +0000

    feat: read and transact

--- AlchemyCommunicationTest/index.js
+++ AlchemyCommunicationTest/index.js
@@ -1,7 +1,54 @@
 import { ethers } from "ethers";
 
-const url =
-  "https://eth-sepolia.g.alchemy.com/v2/iLvdfFWSVQ60fL1Gzgw-t8Kx8-Rk0vIi";
-const provider = new ethers.JsonRpcProvider(url);
-const blockNumber = await provider.getBlockNumber();
-console.log(blockNumber);
+class Playground {
+  constructor(opts) {
+    this.provider = new ethers.JsonRpcProvider(opts.providerUrl);
+    this.addressToLookAt = opts.addressToLookAt;
+  }
+
+  async printLastBlockNumber() {
+    const blockNumber = await this.provider.getBlockNumber();
+
+    console.log(blockNumber);
+  }
+
+  async printLastContent() {
+    const blockContent = await this.provider.getBlock(
+      await this.printLastBlockNumber(),
+    );
+
+    console.log(blockContent);
+  }
+
+  async printBalance() {
+    const balance = await this.provider.getBalance(this.addressToLookAt);
+
+    console.log(ethers.formatUnits(balance, "ether"));
+  }
+
+  async transfer(opts) {
+    const { signerPrivateKey, to } = opts;
+    const signer = new ethers.Wallet(signerPrivateKey, this.provider);
+    const amount = ethers.parseEther("0.01");
+    const tx = await signer.sendTransaction({ to, value: amount });
+
+    await tx.wait();
+
+    console.log(`Done! ${amount} wei have been transferred to ${to}!`);
+  }
+}
+
+async function main() {
+  const playground = new Playground({
+    providerUrl:
+      "https://eth-sepolia.g.alchemy.com/v2/iLvdfFWSVQ60fL1Gzgw-t8Kx8-Rk0vIi",
+    addressToLookAt: "",
+  });
+
+  await playground.printLastBlockNumber();
+  await playground.printLastContent();
+  await playground.printBalance();
+  await playground.transfer({ signerPrivateKey: "", to: "" });
+}
+
+await main();
