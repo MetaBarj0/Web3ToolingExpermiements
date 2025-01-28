8b55c8ff291012ac731ad655aea934c7ca6e5dbf
MetaBarj0 <sebastien.levy.entrepreneur@gmail.com>
2025-01-28 15:55:28 +0000

    test: chai as promised

--- HardHat/test/Prime.js
+++ HardHat/test/Prime.js
@@ -1,15 +1,19 @@
-import { expect } from "chai";
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 import hardhat from "hardhat";
 
+chai.use(chaiAsPromised);
+
 const { ethers } = hardhat;
+chai.should();
 
 describe("Prime contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
     const [owner] = await ethers.getSigners();
 
-    const hardhatToken = await ethers.deployContract("Prime");
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    const contract = await ethers.deployContract("Prime");
+    const ownerBalance = await contract.balanceOf(owner.address);
 
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    contract.totalSupply().should.eventually.equal(ownerBalance);
   });
 });
