99720c78eec016cf99379d2f4f7f2d9c5a92630e
MetaBarj0 <sebastien.levy.entrepreneur@gmail.com>
2025-01-28 17:55:36 +0000

    test: refactoring a bit
    Deployment
    Transactions
    missing test cases

--- HardHat/test/Prime.js
+++ HardHat/test/Prime.js
@@ -7,7 +7,7 @@ chai.use(chaiAsPromised);
 const { ethers } = hardhat;
 chai.should();
 
-describe("Prime contract", function () {
+describe("Prime contract", () => {
   let owner;
   let contract;
 
@@ -16,40 +16,44 @@ describe("Prime contract", function () {
     contract = await ethers.deployContract("Prime");
   });
 
-  it("should set the correct owner", async () => {
-    await contract.owner().should.eventually.equal(owner.address);
+  describe("Deployment", () => {
+    it("should set the correct owner", async () => {
+      await contract.owner().should.eventually.equal(owner.address);
+    });
+
+    it("should assign the total supply of tokens to the owner when deployed", async () => {
+      const ownerBalance = await contract.balanceOf(owner.address);
+
+      await contract.totalSupply().should.eventually.equal(ownerBalance);
+    });
   });
 
-  it("should assign the total supply of tokens to the owner when deployed", async () => {
-    const ownerBalance = await contract.balanceOf(owner.address);
+  describe("Transactions", () => {
+    it("should be able to transfer token between two accounts", async () => {
+      const [_, account1, account2] = await ethers.getSigners();
 
-    await contract.totalSupply().should.eventually.equal(ownerBalance);
+      await contract.transfer(
+        account1.address,
+        await _parsePrime("13"),
+      );
+
+      await contract.connect(account1).transfer(
+        account2.address,
+        await _parsePrime("2"),
+      );
+
+      await contract.balanceOf(account1.address).should.eventually.equal(
+        await _parsePrime("11"),
+      );
+      await contract.balanceOf(account2.address).should.eventually.equal(
+        await _parsePrime("2"),
+      );
+    });
+
+    async function _parsePrime(amount) {
+      const decimal = await contract.decimal();
+
+      return ethers.parseUnits(amount, decimal);
+    }
   });
-
-  it("should be able to transfer token between two accounts", async () => {
-    const [_, account1, account2] = await ethers.getSigners();
-
-    await contract.transfer(
-      account1.address,
-      await _parsePrime("13"),
-    );
-
-    await contract.connect(account1).transfer(
-      account2.address,
-      await _parsePrime("2"),
-    );
-
-    await contract.balanceOf(account1.address).should.eventually.equal(
-      await _parsePrime("11"),
-    );
-    await contract.balanceOf(account2.address).should.eventually.equal(
-      await _parsePrime("2"),
-    );
-  });
-
-  async function _parsePrime(amount) {
-    const decimal = await contract.decimal();
-
-    return ethers.parseUnits(amount, decimal);
-  }
 });
